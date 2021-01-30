afa584b7a0ae56c9be405d76f4b54c9ffdef1c49
Mitnick <wsq150@163.com>
2021-01-30 12:27:47 +0800

    two

---
+++ src/main/java/com/nick/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.nick;
+
+/**
+ * Created by Nick on 2021/1/30 12:25
+ **/
+
+public class HelloWorld {
+    public static void main(String[] ars){
+        System.out.println("hello world");
+    }
+
+}
